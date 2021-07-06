959569a9b20bda96fae133cc77cbaa9fce9cade9
Alhadis <gardnerjohng@gmail.com>
2021-07-07 01:47:33 +1000

    Try to shut Electron up

--- run-ci.sh
+++ run-ci.sh
@@ -1,29 +1,6 @@
 #!/bin/sh
 set -e
 
-eval "`curl -sL https://git.io/fji1w`"
-
-if [ -f /home/travis/.travis/functions ]; then
-	startFold 'fuck' 'Catting functions'
-		cat /home/travis/.travis/functions
-	endFold
-elif [ "$TRAVIS_JOB_ID" ]; then
-	echo 'No such file: /home/travis/.travis/functions'
-fi
-
-startFold 'diagnostics' 'Dumping diagnostic info and shit'
-	startFold 'location' 'CWD and PATH'
-		echo "\$PWD:  $PWD"
-		echo "\$PATH: $PATH"
-	endFold 'location'
-
-	startFold 'ls-cwd'
-		ls -alh
-	endFold 'ls-cwd'
-
-	startFold 'ls-defs' 'Variable and function definitions'
-		node -p 'Object.keys(process.env).sort().map(x =>
-			`\x1B[32m${x}\x1B[39m\x1B[2m=\x1B[36m\x27\x1B[22m${process.env[x]}\x1B[2m\x27\x1B[0m`
-		).join("\n")'
-
-endFold 'diagnostics'
+exec 3>&1; curl -sL https://git.io/fji1w | { sh 2>&1 >&3 3>&- || exit $?; } \
+| grep -v 3>&- 'Electron: Loading non-context-aware native module in renderer:'
+exec 3>&-
