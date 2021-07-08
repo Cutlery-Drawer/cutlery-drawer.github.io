ec6b2f400d30326d255dd44140024d3af5a6c5d0
Alhadis <gardnerjohng@gmail.com>
2021-07-09 00:00:18 +1000

    GitHub Actions just keeps getting better and better
    Not.

--- run-ci.sh
+++ run-ci.sh
@@ -19,8 +19,10 @@ if test -t 2
 	else echo 'STDERR is NOT attached to a terminal'
 fi
 
-printf '\r::error::Foo bar, etc'
-printf '\nFoo\r\r::warning::Foo bar, etc'
+printf 'Foo\033[1GBar\n'
+printf '[ABC\b\b\b]\n'
+printf '[command]/bin/echo "This just gets worse and worse"'
+printf '\nFoo\n\r::warning::Foo bar, etc'
 exit 0
 
 exec 3>&1; curl -sL https://git.io/fji1w | { sh 2>&1 >&3 3>&- || exit $?; } \
