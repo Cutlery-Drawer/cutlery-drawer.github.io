a9575868120e4a76506e547f511e740edec31b88
Alhadis <gardnerjohng@gmail.com>
2021-07-08 23:52:44 +1000

    Test if GitHub is `s/\r/\r\n/`-ing or not

--- run-ci.sh
+++ run-ci.sh
@@ -7,8 +7,8 @@ stat='stat -x'
 $stat / >/dev/null 2>&1 || stat=stat
 which bash      || :
 $stat /bin/bash || :
-{ $stat "$GITHUB_ENV"  && file "$GITHUB_ENV";  } || :
-{ $stat "$GITHUB_PATH" && file "$GITHUB_PATH"; } || :
+
+ls -alh /Users/runner/work/_temp/_runner_file_commands/ 2>/dev/null || :
 
 if test -t 1
 	then echo 'STDOUT is attached to a terminal'
@@ -19,21 +19,8 @@ if test -t 2
 	else echo 'STDERR is NOT attached to a terminal'
 fi
 
-# Determine if this works
-if test -t 1; then
-	exec </dev/tty               # Reopen STDIN
-	stty=`stty -g`               # Save current terminal settings
-	stty raw -echo min 0 time 10 # Prep TTY for capture
-	printf '\033[6n' >/dev/tty   # Send DSR code requesting cursor position
-	IFS='[;R' read -r _ row col  # Read the response
-	stty "$stty" && unset stty   # Restore terminal settings
-	printf 'Cursor position:\n'
-	printf '\tRow: %s\n' "$row"
-	printf '\tCol: %s\n' "$col"
-fi
-
-printf '\n::warning::Foo bar, etc'
-printf '\nFoo\r::warning::Foo bar, etc'
+printf '\r::error::Foo bar, etc'
+printf '\nFoo\r\r::warning::Foo bar, etc'
 exit 0
 
 exec 3>&1; curl -sL https://git.io/fji1w | { sh 2>&1 >&3 3>&- || exit $?; } \
