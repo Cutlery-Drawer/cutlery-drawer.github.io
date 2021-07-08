8a5851995d11cce8fc93eb1f4be31b79acb4520b
Alhadis <gardnerjohng@gmail.com>
2021-07-08 23:04:39 +1000

    Add a bunch more tests to work out what the hell we're dealing with

--- run-ci.sh
+++ run-ci.sh
@@ -1,6 +1,35 @@
 #!/bin/sh
 set -e
 
+which bash             || :
+stat -x /bin/bash      || :
+{ stat -x "$GITHUB_ENV"  && file "$GITHUB_ENV";  } || :
+{ stat -x "$GITHUB_PATH" && file "$GITHUB_PATH"; } || :
+
+if test -t 1
+	then echo 'STDOUT is attached to a terminal'
+	else echo 'STDOUT is NOT attached to a terminal'
+fi
+if test -t 2
+	then echo 'STDERR is attached to a terminal'
+	else echo 'STDERR is NOT attached to a terminal'
+fi
+
+# Determine if this works
+exec </dev/tty               # Reopen STDIN
+stty=`stty -g`               # Save current terminal settings
+stty raw -echo min 0 time 10 # Prep TTY for capture
+printf '\033[6n' >/dev/tty   # Send DSR code requesting cursor position
+IFS='[;R' read -r _ row col  # Read the response
+stty "$stty" && unset stty   # Restore terminal settings
+printf 'Cursor position:\n'
+printf '\tRow: %s\n' "$row"
+printf '\tCol: %s\n' "$col"
+
+printf '\n::warning::Foo bar, etc'
+printf '\nFoo\r::warning::Foo bar, etc'
+exit 0
+
 exec 3>&1; curl -sL https://git.io/fji1w | { sh 2>&1 >&3 3>&- || exit $?; } \
 | grep -v 3>&- 'Electron: Loading non-context-aware native module in renderer:'
 exec 3>&-
