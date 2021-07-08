1c4b12bdce073f10d7f29015131ebb5678536c2b
Alhadis <gardnerjohng@gmail.com>
2021-07-09 02:52:16 +1000

    Test GitHub's `::debug::` thingamajig

--- run-ci.sh
+++ run-ci.sh
@@ -19,11 +19,11 @@ if test -t 2
 	else echo 'STDERR is NOT attached to a terminal'
 fi
 
-printf 'Foo\033[1GBar\n'
-printf '[ABC\b\b\b]\n'
-printf '[command]/bin/echo "This just gets worse and worse"'
-printf '\nFoo\n\r::warning::Foo bar, etc'
-exit 0
+printf 'Form\ffeed\n'
+echo "::debug::Debug-level message"
+echo "::warning::Warning or something"
+echo "::error::Legit error"
+exit
 
 exec 3>&1; curl -sL https://git.io/fji1w | { sh 2>&1 >&3 3>&- || exit $?; } \
 | grep -v 3>&- 'Electron: Loading non-context-aware native module in renderer:'
