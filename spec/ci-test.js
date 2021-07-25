6e6350c260de5a68549f6aeaf6e03c5aae52c161
Alhadis <gardnerjohng@gmail.com>
2021-07-25 19:03:50 +1000

    Send this shit off with one final test

--- spec/ci-test.js
+++ spec/ci-test.js
@@ -2,6 +2,9 @@
 
 describe("Aquí", () => {
 	it("works", () => {
+		console.log({os: process.platform});
+		if(/win/i.test(process.platform))
+			throw new Error("Puto, que chingados fue eso?");
 		console.log("YA ESTA");
 	});
 });
