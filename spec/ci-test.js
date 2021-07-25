29d3a495ded9ccb7acc6688fd069eeaee2d8d39f
Alhadis <gardnerjohng@gmail.com>
2021-07-25 19:12:48 +1000

    MIERDA DE PUTA
    AHUEEEEEVOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- spec/ci-test.js
+++ spec/ci-test.js
@@ -3,7 +3,7 @@
 describe("Aquí", () => {
 	it("works", () => {
 		console.log({os: process.platform});
-		if(/win/i.test(process.platform))
+		if(/^win/i.test(process.platform))
 			throw new Error("Puto, que chingados fue eso?");
 		console.log("YA ESTA");
 	});
