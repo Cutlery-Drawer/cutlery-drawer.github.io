7a6108ec974f67ca380662966f474f5462763835
Alhadis <gardnerjohng@gmail.com>
2021-07-28 18:04:04 +1000

    See what dimmed text looks like

--- stuff.ps1
+++ stuff.ps1
@@ -2,6 +2,8 @@
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 
+$esc = [char]0x1B
+Write-Host "Normal$esc[2mDIMMED$esc[32mGreen$esc[0m$esc[32mGreen$esc[0m"
 Write-Host '[command]' -NoNewline
 Write-Host 'echo Foo Bar Baz'
 
---
+++ stuff.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -e
+
+printf 'Normal\e[2mDIMMED\e[32mGreen\e[0m\e[32mGreen\e[0m\n'
