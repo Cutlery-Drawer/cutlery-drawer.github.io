3c10521ea8ee86ed9195c5336e03c8c7d11723c8
Alhadis <gardnerjohng@gmail.com>
2021-08-08 05:11:13 +1000

    Have a crack at using AppVeyor artefacts

--- spec/ci-test.js
+++ spec/ci-test.js
@@ -1,10 +1,27 @@
 "use strict";
 
+const {join, resolve} = require("path");
+const pkgRoot = resolve(__dirname, "..");
+
 describe("Aquí", () => {
 	it("works", () => {
-		console.log({os: process.platform});
-		if(/^win/i.test(process.platform))
-			throw new Error("Puto, que chingados fue eso?");
 		console.log("YA ESTA");
+	});
+});
+
+describe("Screenshots", () => {
+	before(async () => {
+		await atom.workspace.open(join(pkgRoot, "package.json"));
+		await atom.workspace.open(join(pkgRoot, "README.md"));
+	});
+	
+	it("takes a selfie", async () => {
+		throw new Error("I hate selfies");
+	});
+	
+	it("says hello", async () => {
+		const editor = await atom.workspace.open();
+		editor.shouldPromptToSave = () => false;
+		editor.setText("Hola Mundo");
 	});
 });
